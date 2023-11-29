84ea7063305a05b11a5a00a65a6aec3f1cf10ff4
tarickali <tarickali97@gmail.com>
2023-11-28 20:32:13 -0500

    Adding skeleton main.py for later use.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 """
 title : main.py
 create : @tarickali 23/11/19
-update : @tarickali 23/11/22
+update : @tarickali 23/11/28
 """
 
 from examples.xor import xor_driver
 from examples.mnist import mnist_driver
+from examples.regression import regression_driver
 
 
 def main():
+    ...
     # xor_driver()
-    mnist_driver()
+    # mnist_driver()
+    # regression_driver()
 
 
 if __name__ == "__main__":
